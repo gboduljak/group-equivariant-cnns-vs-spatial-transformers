360a9fb09906bd999049233b5ee9352367aa07cd
Gabrijel Boduljak <gboduljak@outlook.com>
2023-09-10 20:45:38 +0200

    minor

--- groups/discrete_group.py
+++ groups/discrete_group.py
@@ -6,5 +6,5 @@ class DiscreteGroup(torch.nn.Module):
   def __init__(self, order, identity, device):
     super().__init__()
     self.order = order
-    self.register_buffer("identity", torch.Tensor(identity, device=device))
+    self.register_buffer("identity", torch.tensor(identity, device=device))
     self.device = device
